6340ec27cefe003767370a029f44c6b84bc7ab9c
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2022-05-22 22:44:09 -0400

    feat: :sparkles: Can login with sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Try credentials
+        if(!auth()->attempt(request(['email', 'password'])))
+        {
+            return response(['message' => 'Unauthorized'], 401);
+        }
+
+        // Credentials OK
+        $user = auth()->user();
+        $newToken = $user->createToken('token')->plainTextToken;
+
+        // Create cookie token
+        // Not now. Just acces_token to localStorage
+        // $cookie = cookie('jwt', $newToken, -1); // Token last forever
+
+        return response([
+                'message' => 'Success',
+                'access_token' => $newToken
+            ]);
+    }
+
+    public function me(Request $request)
+    {
+        return auth()->user();
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response(['message' => 'Success']);
+    }
+}
